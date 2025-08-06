a70716966f53030607b4f47be302f555977d9385
Dingyuan Huang <huang_dingyuan@student.smc.edu>
2025-08-06 08:01:26 -0700

    PersonalAssistant Constructor

---
+++ assistant.js
@@ -0,0 +1,6 @@
+// Constructor for a personal assistant
+function PersonalAssistant(name) {
+  this.name = name;           // Assistant’s name
+  this.tasks = [];            // List of pending tasks
+  this.mood = "neutral";      // Initial mood
+}
